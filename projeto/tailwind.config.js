5ca90cee213d91fbcbbf25b23edf80fb906668e4
Kauã Gomes <kauaoficialkr@outlook.com>
2024-08-04 18:01:23 -0300

    login 90% pronto

--- projeto/tailwind.config.js
+++ projeto/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: '#00466A'
+      }
+    },
   },
   plugins: [],
 }
